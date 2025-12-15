950de4d25b3ae0b4d697feb565b1b2f91c077f73
Prathamesh Mahindrakar <116503362+prathamesh-mahindrakar@users.noreply.github.com>
2025-12-15 20:55:59 +0530

    Update DashboardRest.java

--- src/main/java/com/cafe/rest/DashboardRest.java
+++ src/main/java/com/cafe/rest/DashboardRest.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RequestMapping("/dashboard")
 public interface DashboardRest {
 	
 	@GetMapping("details")
 	public ResponseEntity<Map<String, Object>> getCount();
 }
+
