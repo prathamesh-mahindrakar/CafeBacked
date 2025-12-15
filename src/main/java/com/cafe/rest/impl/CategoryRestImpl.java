3cb0b6542f083b55cb1454e3959110ea7c8e63d6
Prathamesh Mahindrakar <116503362+prathamesh-mahindrakar@users.noreply.github.com>
2025-12-15 21:42:23 +0530

    Reformat CategoryRestImpl with consistent imports

--- src/main/java/com/cafe/rest/impl/CategoryRestImpl.java
+++ src/main/java/com/cafe/rest/impl/CategoryRestImpl.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 import com.cafe.POJO.Category;
 import com.cafe.contants.CafeContants;
@@ -55,4 +57,5 @@ public class CategoryRestImpl implements CategoryRest {
 	
 
 }
+
 
