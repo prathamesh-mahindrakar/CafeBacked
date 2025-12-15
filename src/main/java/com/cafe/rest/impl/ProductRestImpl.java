87550b4328f2defa3d0f63a3d646457980e735fc
Prathamesh Mahindrakar <116503362+prathamesh-mahindrakar@users.noreply.github.com>
2025-12-15 21:43:05 +0530

    Refactor ProductRestImpl for improved readability

--- src/main/java/com/cafe/rest/impl/ProductRestImpl.java
+++ src/main/java/com/cafe/rest/impl/ProductRestImpl.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 import com.cafe.contants.CafeContants;
 import com.cafe.rest.ProductRest;
@@ -98,4 +100,5 @@ public class ProductRestImpl implements ProductRest{
 	}
 
 }
+
 
